d0724e19ca3d5dc732b04f8ed45accaadfb4f939
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-10-31 22:51:21 -0400

    adding-some-relavent-comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import logo from "../assets/logo/BrainFlix-logo.svg";
 import mohanImage from "../assets/images/Mohan-muruge.jpg";
 import "../styles/Header.scss";
 import Btn from "./Btn";
-import upload from '.././assets/icons/upload.svg';
+import upload from ".././assets/icons/upload.svg";
 import search from ".././assets/icons/search.svg";
 
-
+//header including logo input button image
 export default function Header() {
   return (
     <header className="main-header">
@@ -15,30 +15,40 @@ export default function Header() {
       </h1>
       <form className="main-header__form">
         <div className="main-header__form-input-img">
-        <div className="main-header__form-input-search-img">
-        <img className="main-header__form-input-search-img--element" src={search} alt="search-image" />
-          <input
-            className="main-header__form-input"
-            type="text"
-            id="search"
-            name="search"
-            placeholder="Search"
+          <div className="main-header__form-input-search-img">
+            <img
+              className="main-header__form-input-search-img--element"
+              src={search}
+              alt="search-image"
+            />
+            <input
+              className="main-header__form-input"
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search"
+            />
+          </div>
+          {/* upload button for tablet and desktop */}
+          <div className="Btn__upload2">
+            <img
+              className="Btn__upload2--img"
+              src={upload}
+              alt="upload-image"
+            />
+            <Btn text="UPLOAD" />
+          </div>
+          <img
+            className="main-header__form-img"
+            src={mohanImage}
+            alt="profile image"
           />
         </div>
-        <div className="Btn__upload2">
-        <img className="Btn__upload2--img" src={upload} alt="upload-image" />
-        <Btn text="UPLOAD" />
-        </div>
-        <img className="main-header__form-img" src={mohanImage} alt="profile image" />
-
-        </div>
+        {/* upload button for mobile */}
         <div className="Btn__upload">
-        <img className="Btn__upload--img" src={upload} alt="upload-image" />
-        <Btn text="UPLOAD" />
+          <img className="Btn__upload--img" src={upload} alt="upload-image" />
+          <Btn text="UPLOAD" />
         </div>
-        {/* <button className="main-header__form-button" type="submit">
-          UPLOAD
-        </button> */}
       </form>
     </header>
   );

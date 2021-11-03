9cae4a60fbb78a1cf4a4e22bcaa7d2ba944ce2d4
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-11-03 01:12:08 -0400

    mobile-display--upload-page

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,5 +1,6 @@
 
 import publish from "../assets/icons/publish.svg";
+import thumbnail from "../assets/icons/uploadVideoPreview.jpg";
 import "../styles/UploadPage.scss"
 import Btn from "../components/Btn";
 import Header from "../components/Header";
@@ -7,21 +8,23 @@ import Header from "../components/Header";
 function UploadPage() {
   return (
     <section className="upload">
+    <div className="upload__header">
       <Header />
+      </div>
       <div>
         <h1 className="upload__heading">Upload Video</h1>
         <p className="upload__heading-img">VIDEO THUMBNAIL</p>
-        <img className="upload__img" src="" alt="video-thumbnail-image" />
+        <img className="upload__img" src={thumbnail} alt="video-thumbnail-image" />
       </div>
       <form className="upload__form">
         <label className="upload__form-label" type="text">
           TITLE YOUR VIDEO
         </label>
-        <input className="upload__form-input" type="text" />
+        <input className="upload__form-input" type="text" placeholder="Add a title to your video" />
         <label className="upload__form-label" type="text">
           ADD A VIDEO DESCRIPTION
         </label>
-        <textarea className="upload__form-textarea" type="text" />
+        <textarea className="upload__form-textarea" type="text" placeholder="Add a description to your video"/>
         <button className="upload__form-button">
           <img src={publish} alt="video-thumbnail-image" />
           <Btn />

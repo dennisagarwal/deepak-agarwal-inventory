b5ee2047b1fe2d7b9fb9a132c35909a075bb9705
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-11-07 18:57:04 -0500

    levelling-user-comment-and-next-video-in-desktop-view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import upload from ".././assets/icons/upload.svg";
 import search from ".././assets/icons/search.svg";
 import { Link } from "react-router-dom";
 //sprint2
-//header including logo input button image
+//header including logo, input, button ,image
 export default function Header() {
   return (
     <header className="main-header">
@@ -30,25 +30,20 @@ export default function Header() {
               placeholder="Search"
             />
           </div>
-          {/* upload button for tablet and desktop */}
-          {/* <Link to="/UploadPage"> */}
           <Link to="/uploads" className="Btn__upload2">
             <img
               className="Btn__upload2--img"
               src={upload}
               alt="upload-image"
             />
-            {/* sprint2 */}
            <Btn text="UPLOAD" />
           </Link>
-          {/* </Link> */}
           <img
             className="main-header__form-img"
             src={mohanImage}
             alt="profile image"
           />
         </div>
-        {/* upload button for mobile */}
         <div className="Btn__upload">
           <img className="Btn__upload--img" src={upload} alt="upload-image" />
           <Btn text="UPLOAD" />
